e5812edb4e5cd3eb0d7a5287cfa24ad31e5253a8
furkannibis <ibis.furkann@gmail.com>
2024-10-31 20:24:20 +0300

    Bento grid sorunu

--- components/bento.tsx
+++ components/bento.tsx
@@ -1,7 +1,7 @@
 export const BentoGrid = () => {
     return (
         <section
-            className='bento grid-xl-layout sm:grid-sm-layout xl:grid-xl-layout mx-auto grid max-w-[375px] grid-cols-2 gap-2 *:rounded-3xl *:bg-no-repeat sm:max-w-screen-sm xl:max-w-screen-xl xl:grid-cols-4'>
+            className='bento grid-mobile-layout sm:grid-sm-layout xl:grid-xl-layout mx-auto grid max-w-[375px] grid-cols-2 gap-2 *:rounded-3xl *:bg-no-repeat sm:max-w-screen-sm xl:max-w-screen-xl xl:grid-cols-4'>
             <div className="grid-item-a aspect-square rounded-3xl bg-cover bg-center bg-no-repeat sm:aspect-[2.1/1] xl:aspect-auto" role="img" aria-label="introduction" rel="preload"></div>
             <div className='grid-item-b bg-black aspect-square'></div>
             <div className='grid-item-c bg-center bg-no-repeat bg-cover aspect-[1/2.1] xl:aspect-auto' aria-hidden='true' />
